41f63401d1b02f167399257bc494a18c09b3b36b
Max McLaren <maxtova@yahoo.com>
2023-11-25 13:54:10 -0700

    completed styles.css with form taking up an entire right side of page

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 function validatePassword() {
     let password = document.querySelector("#password").value;
     let passConfirm = document.querySelector("#pass-confirm").value;
+    let pwdMatch = document.querySelector("#pwd-match-msg");
 
     if (password !== passConfirm) {
-        alert("Passwords do not match!");
+        pwdMatch.textContent = "Passwords do not match!";
         return false;
     }
-
+    pwdMatch.textContent = "Passwords match";
     return true;
 }
